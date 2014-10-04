2f3cfaabe3318ffc53b2b57b842c4a87e2d7a05e
Francisco Baio Dias <xicombd@gmail.com>
2014-10-04 15:21:50 +0100

    Added user handlers

---
+++ server/routes/user/handlers.js
@@ -0,0 +1,122 @@
+var Joi = require('joi');
+var log = require('server/helpers/logger');
+
+
+var handlers = module.exports;
+
+exports.create = {
+  validate: {
+    payload: {
+      id: Joi.string().description('Id of the user'),
+      name: Joi.string().required().description('Name of the user'),
+      role: Joi.string().description('Role of the user'),
+      mail: Joi.string().email().required().description('Mail of the user'),
+      facebook: {
+        id: Joi.string(),
+        token: Joi.string(),
+      },
+      fenix: {
+        id: Joi.string(),
+        token: Joi.string(),
+        refreshToken: Joi.string(),
+      },
+      points: {
+        available: Joi.number(),
+        total: Joi.number()
+      },
+      achievements: [{
+        id: Joi.string(),
+        date: Joi.date(),
+      }]
+    }
+  },
+  pre: [
+    { method: 'user.create(payload)', assign: 'user' }
+  ],
+  handler: function (request, reply) {
+    reply(request.pre.user).created('/api/user/'+request.pre.user.id);
+  },
+  description: 'Creates a new user'
+};
+
+
+exports.update = {
+  validate: {
+    params: {
+      id: Joi.string().required().description('Id of the user we want to update'),
+    },
+    payload: {
+      id: Joi.string().description('Id of the user'),
+      name: Joi.string().description('Name of the user'),
+      role: Joi.string().description('Role of the user'),
+      mail: Joi.string().email().description('Mail of the user'),
+      facebook: {
+        id: Joi.string(),
+        token: Joi.string(),
+      },
+      fenix: {
+        id: Joi.string(),
+        token: Joi.string(),
+        refreshToken: Joi.string(),
+      },
+      points: {
+        available: Joi.number(),
+        total: Joi.number()
+      },
+      achievements: [{
+        id: Joi.string(),
+        date: Joi.date(),
+      }]
+    }
+  },
+  pre: [
+    { method: 'user.update(params.id, payload)', assign: 'user' }
+  ],
+  handler: function (request, reply) {
+    reply(request.pre.user);
+  },
+  description: 'Updates an user'
+};
+
+
+exports.get = {
+  validate: {
+    params: {
+      id: Joi.string().required().description('Id of the user we want to retrieve'),
+    }
+  },
+  pre: [
+    { method: 'user.get(params.id)', assign: 'user' }
+  ],
+  handler: function (request, reply) {
+    reply(request.pre.user);
+  },
+  description: 'Gets an user'
+};
+
+
+exports.list = {
+  pre: [
+    { method: 'user.list()', assign: 'users' }
+  ],
+  handler: function (request, reply) {
+    reply(request.pre.users);
+  },
+  description: 'Gets all the users'
+};
+
+
+exports.remove = {
+  validate: {
+    params: {
+      id: Joi.string().required().description('Id of the user we want to remove'),
+    }
+  },
+  pre: [
+    { method: 'user.remove(params.id)', assign: 'user' }
+  ],
+  handler: function (request, reply) {
+    reply(request.pre.user);
+  },
+  description: 'Removes an user'
+};
---
+++ server/routes/user/methods.js
@@ -0,0 +1,88 @@
+var Boom = require('boom');
+var slug = require('slug');
+var server = require('server').hapi;
+var log = require('server/helpers/logger');
+var dupKeyParser = require('server/helpers/dupKeyParser');
+var User = require('server/models/user');
+
+function create(user, cb) {
+  user.id = user.id || slug(user.name).toLowerCase();
+
+  User.create(user, function(err, _user) {
+    if (err) {
+      if(err.code == 11000) {
+      	log.warn({err:err, user: user.id}, 'user is a duplicate')
+        return cb(Boom.conflict(dupKeyParser(err.err)+' is a duplicate'));
+      }
+
+      log.error({ err: err, user: user.id}, 'error creating user');
+      return cb(Boom.internal());
+    }
+
+    cb(null, _user);
+  });
+};
+
+function update(id, user, cb) {
+  User.findOneAndUpdate({id: id}, user, function(err, _user) {
+    if (err) {
+      log.error({ err: err, user: id}, 'error updating user');
+      return cb(Boom.internal());
+    }
+    if (!_user) {
+      log.error({ err: err, user: id}, 'error updating user');
+      return cb(Boom.notFound());
+    }
+
+    cb(null, _user);
+  });
+};
+
+function get(id, cb) {
+  User.findOne({id: id}, function(err, user) {
+    if (err) {
+      log.error({ err: err, user: id}, 'error getting user');
+      return cb(Boom.internal());
+    }
+    if (!user) {
+      log.error({ err: err, user: id}, 'error getting user');
+      return cb(Boom.notFound());
+    }
+
+    cb(null, user);
+  });
+};
+
+function list(cb) {
+  User.find({}, function(err, users) {
+    if (err) {
+      log.error({ err: err}, 'error getting all users');
+      return cb(Boom.internal());
+    }
+    
+    cb(null, users);
+  });
+};
+
+function remove(id, cb) {
+  User.findOneAndRemove({id: id}, function(err, user){
+    if (err) {
+      log.error({ err: err, user: id}, 'error deleting user');
+      return cb(Boom.internal());
+    }
+    if (!user) {
+      log.error({ err: err, user: id}, 'error deleting user');
+      return cb(Boom.notFound());
+    }
+
+    return cb(null, user);
+  });
+};
+
+
+
+server.method('user.create', create, {});
+server.method('user.update', update, {});
+server.method('user.get', get, {});
+server.method('user.list', list, {});
+server.method('user.remove', remove, {});
