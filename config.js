6ef1385ad8bc190866b938c6df1d04998ebb93c9
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2015-02-03 02:33:19 +0000

    proper config

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ config.bunyan = {
 };
 
 config.logs = {
-  path: process.env.CANNON_LOG_PATH || '/tmp/logs/',
+  path: process.env.CANNON_LOG_PATH || __dirname + '/tmp/logs/',
 };
 
 config.swagger = {
@@ -52,10 +52,22 @@ config.swagger = {
 
 config.auth = {};
 
+config.auth.paths = {
+  tokenKeys: {
+    pub: process.env.CANNON_TOKEN_PRIVATE_KEY || __dirname + '/keys/token.pub',
+    priv: process.env.CANNON_TOKEN_PUBLIC_KEY || __dirname + '/keys/token.key'
+  }
+};
+
+config.auth.paths.refreshKeys = {
+  pub: process.env.CANNON_REFRESH_TOKEN_PRIVATE_KEY || config.auth.paths.tokenKeys.pub,
+  priv: process.env.CANNON_REFRESH_TOKEN_PUBLIC_KEY || config.auth.paths.tokenKeys.priv
+};
+
 config.auth.token = {
   ttl: process.env.CANNON_TOKEN_EXPIRATION || 10, //in minutes
-  privateKey:  fs.readFileSync(process.env.CANNON_TOKEN_PRIVATE_KEY || './keys/token.key'),
-  publicKey: fs.readFileSync(process.env.CANNON_TOKEN_PUBLIC_KEY || './keys/token.pub'),
+  privateKey:  fs.readFileSync(config.auth.paths.tokenKeys.pub),
+  publicKey: fs.readFileSync(config.auth.paths.tokenKeys.priv),
   algorithm: process.env.CANNON_TOKEN_ALGORITHM || 'RS256',
   issuer: process.env.CANNON_TOKEN_ISSUER || 'cannon_masters',
   audience: process.env.CANNON_TOKEN_AUDIENCE || 'audience'
@@ -63,8 +75,8 @@ config.auth.token = {
 
 config.auth.refreshToken = {
   ttl: process.env.CANNON_REFRESH_TOKEN_EXPIRATION || 43200, //in minutes
-  privateKey:  fs.readFileSync(process.env.CANNON_REFRESH_TOKEN_PRIVATE_KEY || './keys/token.key'),
-  publicKey: fs.readFileSync(process.env.CANNON_REFRESH_TOKEN_PUBLIC_KEY || './keys/token.pub'),
+  privateKey:  fs.readFileSync(config.auth.paths.refreshKeys.pub),
+  publicKey: fs.readFileSync(config.auth.paths.refreshKeys.priv),
   algorithm: process.env.CANNON_REFRESH_TOKEN_ALGORITHM || config.auth.token.algorithm,
   issuer: process.env.CANNON_REFRESH_TOKEN_ISSUER || config.auth.token.issuer,
   audience: process.env.CANNON_REFRESH_TOKEN_AUDIENCE || config.auth.token.audience
