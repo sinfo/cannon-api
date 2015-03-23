c5e344397d587cd7211bc509ed3fdc086d36c59f
Makay11 <dpais11@gmail.com>
2015-03-23 16:34:37 +0000

    created the files for the new plugin

---
+++ server/plugins/surveyResults/index.js
@@ -0,0 +1,43 @@
+var Hoek = require('hoek');
+var Path = require('path');
+var Handlebars = require('handlebars');
+var log = require('server/helpers/logger');
+
+var handlers = require('./handlers');
+
+// Declare internals
+var internals = {
+  defaults: {
+    endpoint: '/templates',
+    auth: false,
+    basePath: Path.join(__dirname, 'templates'),
+    partialsPath: Path.join(__dirname, 'templates'),
+  }
+};
+
+exports.register = function (plugin, options, next) {
+
+  var settings = Hoek.clone(internals.defaults);
+  Hoek.merge(settings, options);
+
+  plugin.views({
+    engines: settings.engines || {
+      hbs: {
+        module: Handlebars
+      }
+    },
+    path: settings.basePath,
+  });
+
+  plugin.route({
+    method: 'GET',
+    path: settings.endpoint+'/achievements/{achievementId}',
+    config: handlers.grid
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
