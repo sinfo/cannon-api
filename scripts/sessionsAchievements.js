d84dd46e67c6c84186c8773a6f90e00e86cc86f2
johnytiago <joaosao10@gmail.com>
2018-02-26 01:54:28 +0000

    Achievements script update

--- scripts/sessionsAchievements.js
+++ scripts/sessionsAchievements.js
@@ -2,18 +2,32 @@ const server = require('../server').hapi
 const API = server.methods
 const log = require('../server/helpers/logger')
 const async = require('async')
-const EVENT = '23-sinfo-conf'
+const EVENT = '25-sinfo'
 
 API.session.list({event: EVENT}, (err, sessions) => {
   log.debug({err: err, count: sessions.length}, 'got sessions')
 
   async.each(sessions, (session, cb) => {
+    let achievementValue = 0
+    switch(session.kind) {
+      case "Keynote":
+        achievementValue = 10
+      break;
+      case "Workshop":
+        achievementValue = 20
+      break;
+      case "Presentation":
+        achievementValue = 30
+      break;
+      default:
+        achievementValue = 10
+    }
     const sessionAchievement = {
       'name': `Went to "${session.name}"`,
       'id': 'session-' + session.id,
       'session': session.id,
-      'value': 10,
-      'img': `http://static.sinfo.org/SINFO_23/achievements/apresentacoes/session-${session.id}.png`
+      'value': achievementValue,
+      'img': `http://static.sinfo.org/SINFO_25/achievements/${session.kind.toLowerCase()}/session-${session.id}.png`
     }
 
     // log.debug({sessionAchievement: sessionAchievement}, 'creating achievement');
