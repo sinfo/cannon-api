938f38555094f0668162bad23a56ed71abfa3e9c
fchamicapereira <fchamicapereira@gmail.com>
2019-02-15 16:56:55 +0000

    fix bug in script

--- scripts/sessionsAchievements.js
+++ scripts/sessionsAchievements.js
@@ -9,26 +9,32 @@ API.session.list({event: EVENT}, (err, sessions) => {
   
   async.each(sessions, (session, cb) => {
     let achievementValue = 0
+    let kind = ''
+
     switch (session.kind) {
       case 'Keynote':
         achievementValue = 10
+        kind = 'keynote'
         break
       case 'Workshop':
         achievementValue = 20
+        kind = 'workshop'
         break
       case 'Presentation':
         achievementValue = 30
+        kind = 'presentations'
         break
       default:
         achievementValue = 10
     }
+    
 
     const sessionAchievement = {
       'name': `Went to "${session.name}"`,
       'id': 'session-' + session.id,
       'session': session.id,
       'value': achievementValue,
-      'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/${session.kind.toLowerCase()}/${session.id}.png`,
+      'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/${kind.toLowerCase()}/${session.id}.png`,
       'validity': {
         'from': new Date(),
         'to': new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
@@ -42,32 +48,32 @@ API.session.list({event: EVENT}, (err, sessions) => {
         log.warn({err: err, sessionAchievement: sessionAchievement}, 'achievement')
       }
 
-      cb()
+      const sessionAchievement = {
+        'name': `Submitted CV`,
+        'id': 'submitted-cv-' + EVENT,
+        'value': 0,
+        'kind': 'cv',
+        'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/cv/cv.png`,
+        'validity': {
+          'from': new Date(),
+          'to': new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
+        }
+      }
+    
+      // log.debug({sessionAchievement: sessionAchievement}, 'creating achievement');
+    
+      API.achievement.create(sessionAchievement, (err, achievement) => {
+        if (err) {
+          log.warn({err: err, sessionAchievement: sessionAchievement}, 'achievement')
+        }
+    
+        cb()
+      })
+    
     })
+
   }, (err) => {
     log.info({err: err}, 'achievements created')
-
-    const sessionAchievement = {
-      'name': `Submitted CV`,
-      'id': 'submitted-cv-' + EVENT,
-      'value': 0,
-      'kind': 'cv',
-      'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/cv/cv.png`,
-      'validity': {
-        'from': new Date(),
-        'to': new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
-      }
-    }
-  
-    // log.debug({sessionAchievement: sessionAchievement}, 'creating achievement');
-  
-    API.achievement.create(sessionAchievement, (err, achievement) => {
-      if (err) {
-        log.warn({err: err, sessionAchievement: sessionAchievement}, 'achievement')
-      }
-  
-      cb()
-    })
 
     process.exit(0)
   })
