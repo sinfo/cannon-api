a29b0f06c900562777d57a4ef0a798f0ee9076a1
fchamicapereira <fchamicapereira@gmail.com>
2019-02-16 00:22:20 +0000

    major change on the point system

--- scripts/sessionsAchievements.js
+++ scripts/sessionsAchievements.js
@@ -2,80 +2,241 @@ const server = require('../server').hapi
 const API = server.methods
 const log = require('../server/helpers/logger')
 const async = require('async')
+const request = require('request')
+
 const EVENT = '26-sinfo'
+const EVENT_START = new Date(2019, 01, 18, 0, 0, 0, 0)
+const CORLIEF = 'https://api.corlief.sinfo.org/reservation/latest'
+const DECK_USER = 'francisco.pereira'
+const DECK_TOKEN = 'XMO7vIyMdp0Xffd1eh5ZjoLEdX9vVZ09MRyDcgkZXSaxUJPXzB9lKAiRRqpW4fyYHdV2yXPMzOFr29MQtmAZjVXHFeyqsFOfZfoK'
 
-API.session.list({event: EVENT}, (err, sessions) => {
-  log.debug({err: err, count: sessions.length}, 'got sessions')
+// fill the true values of validity, or just make them all valid now
+const trueValidity = true
+
+const cvFrom = EVENT_START
+const cvTo = new Date(2019, 02, 31, 23, 59, 59, 999)
+
+// points distribution (%)
+const points = {
+  workshops: 0.4,
+  presentations: 0.3,
+  stands: 0.2,
+  keynote: 0.1
+}
+
+const totalPoints = 1000.0
+
+const achievements = {
+  '1': {
+    stands: [],
+    presentations: [],
+    workshops: [],
+    keynote: []
+  },
   
-  async.each(sessions, (session, cb) => {
-    let achievementValue = 0
-    let kind = ''
+  '2': {
+    stands: [],
+    presentations: [],
+    workshops: [],
+    keynote: []
+  },
+  
+  '3': {
+    stands: [],
+    presentations: [],
+    workshops: [],
+    keynote: []
+  },
+  
+  '4': {
+    stands: [],
+    presentations: [],
+    workshops: [],
+    keynote: []
+  },
+  
+  '5': {
+    stands: [],
+    presentations: [],
+    workshops: [],
+    keynote: []
+  }
+}
 
-    switch (session.kind) {
-      case 'Keynote':
-        achievementValue = 10
-        kind = 'keynote'
-        break
-      case 'Workshop':
-        achievementValue = 20
-        kind = 'workshop'
-        break
-      case 'Presentation':
-        achievementValue = 30
-        kind = 'presentations'
-        break
-      default:
-        achievementValue = 10
-    }
+const otherAchievements = []
+
+function stands(list_of_functions) {
+  request({
+    url: CORLIEF,
+    headers: { 'Authorization': `${DECK_USER} ${DECK_TOKEN}` }
+  }, (err, response, body) => {
+    const reservations = JSON.parse(body)
+  
+    reservations.forEach(r => {
+      const companyId = r.companyId
+      
+      if (r.feedback === undefined || r.feedback.status !== 'CONFIRMED')  return
+      
+      for(let stand of r.stands) {
+        const day = stand.day
+        const from = new Date(EVENT_START.getTime() + (1000 * 60 * 60 * 24) * (day - 1)) // shift the day
+        const to   = new Date(from)
+  
+        to.setHours(23)
+        to.setMinutes(59)
+        to.setSeconds(59)
+        to.setMilliseconds(999)
+  
+        const img = `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/stands/stands_${companyId}_${day}.png`
+  
+        achievements[day].stands.push({
+          name: `Talked to this company`,
+          id: 'stand-' + companyId + '-' + day,
+          img: img,
+          kind: 'stand',
+          validity: {
+            from: from,
+            to: to
+          }
+        })
+      }
+    })
     
+    const next = list_of_functions.pop()
+    next(list_of_functions)
+  })
+}
 
-    const sessionAchievement = {
-      'name': `Went to "${session.name}"`,
-      'id': 'session-' + session.id,
-      'session': session.id,
-      'value': achievementValue,
-      'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/${kind.toLowerCase()}/${session.id}.png`,
-      'validity': {
-        'from': new Date(),
-        'to': new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
-      }
-    }
+function sessions(list_of_functions) {
+  API.session.list({ event: EVENT }, (err, sessions) => {
+    sessions.forEach(session => {
+      let kind = ''
+      let sessionDay = 0
+      const sessionDate = new Date(session.date)
 
-    // log.debug({sessionAchievement: sessionAchievement}, 'creating achievement');
-
-    API.achievement.create(sessionAchievement, (err, achievement) => {
-      if (err) {
-        log.warn({err: err, sessionAchievement: sessionAchievement}, 'achievement')
-      }
-
-      const sessionAchievement = {
-        'name': `Submitted CV`,
-        'id': 'submitted-cv-' + EVENT,
-        'value': 0,
-        'kind': 'cv',
-        'img': `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/cv/cv.png`,
-        'validity': {
-          'from': new Date(),
-          'to': new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
+      let found = false
+      for (let day = 1; day <= 5; day++) {
+        const eventDay = new Date(EVENT_START.getTime() + (1000 * 60 * 60 * 24) * (day - 1))
+        
+        if (eventDay.getDate() === sessionDate.getDate()) {
+          sessionDay = day
+          found = true
+          break
         }
       }
-    
-      // log.debug({sessionAchievement: sessionAchievement}, 'creating achievement');
-    
-      API.achievement.create(sessionAchievement, (err, achievement) => {
-        if (err) {
-          log.warn({err: err, sessionAchievement: sessionAchievement}, 'achievement')
+
+      if (!found) {
+        log.error({ sessionId: session.id }, 'could not calculate day of the week for this session')
+        return
+      }
+
+      const from = new Date(EVENT_START.getTime() + (1000 * 60 * 60 * 24) * (sessionDay - 1)) // shift the day
+      const to   = new Date(from)
+
+      to.setHours(23)
+      to.setMinutes(59)
+      to.setSeconds(59)
+      to.setMilliseconds(999)
+
+      let achievement = {
+        name: `Went to "${session.name}"`,
+        id: 'session-' + session.id,
+        session: session.id,
+        kind: 'session'
+      }
+
+      achievement['validity'] = !trueValidity ? {
+          from: new Date(),
+          to: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7) // 1 week
+        } : {
+          from: from,
+          to: to
         }
-    
-        cb()
-      })
-    
+
+      switch (session.kind) {
+        case 'Keynote':
+          kind = 'keynote'
+          break
+        case 'Workshop':
+          kind = 'workshops'
+          break
+        case 'Presentation':
+          kind = 'presentations'
+          break
+      }
+
+      achievement['kind'] = 'session'
+      achievement['img'] = `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/${kind.toLowerCase()}/${session.id}.png`
+      achievements[sessionDay][kind].push(achievement)
     })
 
-  }, (err) => {
-    log.info({err: err}, 'achievements created')
+    const next = list_of_functions.pop()
+    next(list_of_functions)
+  })
+}
 
-    process.exit(0)
+function cv(list_of_functions) {
+  otherAchievements.push({
+    name: `Submitted CV`,
+    id: 'submitted-cv-' + EVENT,
+    value: 0,
+    kind: 'cv',
+    img: `https://sinfo.ams3.cdn.digitaloceanspaces.com/static/${EVENT}/achievements/cv/cv.png`,
+    validity: {
+      from: cvFrom,
+      to: cvTo
+    }
+  })
+
+  const next = list_of_functions.pop()
+  next(list_of_functions)
+}
+
+stands([function () {
+  let totalStands = 0
+  let totalPresentations = 0
+  let totalWorkshops = 0
+  let totalKeynotes = 0
+
+  Object.keys(achievements).forEach(day => {
+    const nStands = achievements[day].stands.length
+    const nPresentations = achievements[day].presentations.length
+    const nWorkshops = achievements[day].workshops.length
+    const nKeynotes = achievements[day].keynote.length
+
+    totalStands += nStands
+    totalPresentations += nPresentations
+    totalWorkshops += nWorkshops
+    totalKeynotes += nKeynotes
+    
+    const calculatedPoints = {
+      stands: nStands > 0 ? Math.floor((points.stands * totalPoints) / nStands) : 0,
+      presentations: nPresentations > 0 ? Math.floor((points.presentations * totalPoints) / nPresentations) : 0,
+      workshops: nWorkshops > 0 ? Math.floor((points.workshops * totalPoints) / nWorkshops) : 0,
+      keynote: nKeynotes > 0 ? Math.floor((points.keynote * totalPoints) / nKeynotes) : 0
+    }
+
+    const totalPointsThisDay = calculatedPoints.stands * nStands
+      + calculatedPoints.presentations * nPresentations
+      + calculatedPoints.workshops * nWorkshops
+      + calculatedPoints.keynote * nKeynotes
+
+    console.log(`
+      # stands        ${nStands} (${calculatedPoints.stands})
+      # presentations ${nPresentations} (${calculatedPoints.presentations})
+      # workshops     ${nWorkshops} (${calculatedPoints.workshops})
+      # keynotes      ${nKeynotes} (${calculatedPoints.keynote})
+      Total points this day : ${totalPointsThisDay}
+    `)
+
   })
   
-})
+  console.log(`
+    # stands        ${totalStands}
+    # presentations ${totalPresentations}
+    # workshops     ${totalWorkshops}
+    # keynotes      ${totalKeynotes}
+  `)
+
+  process.exit(0)
+}, sessions, cv])
