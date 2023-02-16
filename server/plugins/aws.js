da1a313ee37b04da737b53701d58e28d6544d0d2
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-02-16 11:45:10 +0000

    refact: Use different AWS library

--- server/plugins/aws.js
+++ server/plugins/aws.js
@@ -1,62 +1,58 @@
-const aws = require('aws-sdk')
+const aws = require('@aws-sdk/client-s3')
 const path = require('path')
 const config = require(path.join(__dirname, '..', '..', 'config'))
 const mime = require('mime-types')
 const log = require('../helpers/logger')
 
-const AWS_URL = `https://${config.aws.storageRegion}.${config.aws.storageDomain}${path}`
-
-function promiseWrapper(s3, key, params, returnData) {
-  return new Promise((resolve, reject) => {
-    s3[key](params, (err, data) => {
-      if (err) {
-        log.error({ error: err })
-        resolve(null)
-      }
-
-      resolve(returnData || data)
-    })
-  })
-}
-
-module.exports.download = (path, filename) => {
-  let s3 = new aws.S3({
-    endpoint: new aws.Endpoint(AWS_URL),
+const s3Client = new aws.S3Client({
+  endpoint: `https://${config.aws.storageRegion}.${config.aws.storageDomain}`,
+  forcePathStyle: false,
+  region: config.aws.storageRegion,
+  credentials: {
     accessKeyId: config.aws.storageKey,
     secretAccessKey: config.aws.storageSecret
-  })
+  }
+})
 
-  return promiseWrapper(s3, 'getObject', {
+module.exports.download = async (path, filename) => {
+  const params = {
     Bucket: config.aws.storageName,
-    Key: filename
-  })
+    Key: path + filename
+  }
+
+  try {
+    await s3Client.send(new aws.GetObjectCommand(params))
+  } catch(err) {
+    log.error({ error: err })
+  }
 }
 
-module.exports.upload = (path, buffer, filename, isPublic) => {
-  const s3 = new aws.S3({
-    endpoint: new aws.Endpoint(AWS_URL),
-    accessKeyId: config.aws.storageKey,
-    secretAccessKey: config.aws.storageSecret
-  })
-
-  return promiseWrapper(s3, 'putObject', {
-    ACL: isPublic !== undefined && isPublic ? 'public-read' : 'authenticated-read',
+module.exports.upload = async (path, buffer, filename, isPublic) => {
+  const params = {
+    Bucket: config.aws.storageName,
+    Key: path + filename,
     Body: buffer,
-    Bucket: config.aws.storageName,
-    Key: filename,
+    ACL: isPublic !== undefined && isPublic ? 'public-read' : 'authenticated-read',
     ContentType: mime.lookup(filename)
-  }, `https://${config.aws.storageName}.${config.aws.storageRegion}.${config.aws.storageDomain}${path}/${filename}`)
+  }
+
+  try {
+    await s3Client.send(new aws.PutObjectCommand(params))
+    return `https://${config.aws.storageName}.${config.aws.storageRegion}.${config.aws.storageDomain}/${path}${filename}`
+  } catch(err) {
+    log.error({ error: err })
+  }
 }
 
-module.exports.delete = (path, filename) => {
-  let s3 = new aws.S3({
-    endpoint: new aws.Endpoint(`https://${config.aws.storageRegion}.${config.aws.storageDomain}${path}`),
-    accessKeyId: config.aws.storageKey,
-    secretAccessKey: config.aws.storageSecret,
-  })
-
-  return promiseWrapper(s3, 'deleteObject', {
+module.exports.delete = async (path, filename) => {
+  const params = {
     Bucket: config.aws.storageName,
-    Key: filename
-  })
+    Key: path + filename
+  }
+
+  try {
+    await s3Client.send(new aws.DeleteObjectCommand(params))
+  } catch(err) {
+    log.error({ error: err })
+  }
 }
