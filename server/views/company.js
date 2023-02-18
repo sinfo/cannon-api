84af4451d4edb65f3dabf7eb290048a61aa40b69
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-02-18 16:24:46 +0000

    feat: Implement views for companies, events, sessions and speakers

--- server/views/company.js
+++ server/views/company.js
@@ -1,17 +1,17 @@
 module.exports = function render (content) {
-    if (content instanceof Array) {
-      return content.map(renderObject)
-    }
-  
-    return renderObject(content)
+  if (content instanceof Array) {
+    return content.map(renderObject)
   }
+
+  return renderObject(content)
+}
   
-  function renderObject (model) {
-    return {
-        id: model.id,
-        name: model.name,
-        advertisementLvl: model.advertisementLvl,
-        img: model.img
-    }
+function renderObject (model) {
+  return {
+      id: model.id,
+      name: model.name,
+      advertisementLvl: model.advertisementLvl,
+      img: model.img
   }
+}
   
